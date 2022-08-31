af804547cbf1aa6177df945354717f07a80658ff
jasonqiu98 <jingxuan.qiu@outlook.com>
2022-08-31 12:09:43 +0200

    update docker-compose for postgres

--- volumes/psql/init/init.sh
+++
@@ -1,9 +0,0 @@
-# create a new database `demo_db`
-psql -p 5432 -U postgres -f /opt/sql/create_db.sql
-
-echo "Database `demo_db` created."
-
-# create schema's, tables and insert data
-psql -p 5432 -U postgres -d demo_db -f /opt/sql/demo_db.sql
-
-echo "Schema's, tables and data are generated."
--- volumes/psql/sql/create_db.sql
+++
@@ -1,2 +0,0 @@
--- create a database called 'demo_db' in the instance of PostgreSQL
-create database demo_db;
